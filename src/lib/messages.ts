13f9c3dc509a2b60d99c3a68d9ce4759a9bcc6dd
murat-aydin01 <murataydin2001@outlook.com>
2025-06-06 19:35:37 +0300

    feat: hata mesajları eklendi

--- src/lib/messages.ts
+++ src/lib/messages.ts
@@ -1,3 +1,5 @@
 export const messages = {
-    emailAlreadyTaken: "E-posta daha önce kullanılmış, başka bir e-posta deneyin."
+    emailAlreadyTaken: "E-posta daha önce kullanılmış, başka bir e-posta deneyin.",
+    invalidCredentials: "Geçersiz e-posta veya şifre",
+    incorrectCredentials: "Hatalı giriş yaptınız."
 }
