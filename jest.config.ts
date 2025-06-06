c50fcf826b52d1d222864e00918498c2a43ccea2
murat-aydin01 <murataydin2001@outlook.com>
2025-06-06 15:24:36 +0300

    chore: test için config ve dosya yapısı düzenlendi

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,19 @@
-import type { Config } from 'jest'
-import nextJest from 'next/jest.js'
- 
+import type {Config} from 'jest';
+import nextJest from 'next/jest.js';
+
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: './',
-})
- 
-// Add any custom config to be passed to Jest
+});
+
 const config: Config = {
-  coverageProvider: 'v8',
-  testEnvironment: 'jsdom',
-  // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-}
- 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
-export default createJestConfig(config)
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  moduleNameMapper: {'^@/components/(.*)$': '<rootDir>/components/$1'},
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testEnvironment: "jsdom",
+  testPathIgnorePatterns: ['/node_modules/', '/tests/playwright/'],
+};
+
+export default createJestConfig(config);
---
+++ tests/jest/page.test.tsx
@@ -0,0 +1,9 @@
+import "@testing-library/jest-dom";
+
+describe("demo", () => {
+  it("x = 1 olmali", () => {
+    const x = 1;
+
+    expect(x).toEqual(1);
+  });
+});
