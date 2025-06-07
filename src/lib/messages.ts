efe3a05ac4bc6da868df7644626c10b9f17370b1
murat-aydin01 <murataydin2001@outlook.com>
2025-06-07 14:55:18 +0300

    feat: session ve unknown error mesajları eklendi

--- src/lib/messages.ts
+++ src/lib/messages.ts
@@ -1,5 +1,7 @@
 export const messages = {
+    unknownError: "Bilinmeyen bir hata oluştu",
     emailAlreadyTaken: "E-posta daha önce kullanılmış, başka bir e-posta deneyin.",
     invalidCredentials: "Geçersiz e-posta veya şifre",
-    incorrectCredentials: "Hatalı giriş yaptınız."
+    incorrectCredentials: "Hatalı giriş yaptınız.",
+    sessionNotFound: "Geçerli oturum bulunamadı. Giriş yapınız."
 }
